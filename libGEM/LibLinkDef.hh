ce10fa4b48d955d00570b8de9c29654ce59acc78
Joseph McKenna <joseph.mckenna@cern.ch>
2021-02-28 11:19:55 +0100

    Move analyzer features into libGEM and add CMakefile (that only links with root)

---
+++ libGEM/LibLinkDef.hh
@@ -0,0 +1,27 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TStoreGEMEventHeader+;
+#pragma link C++ class TLVTimestamp+;
+#pragma link C++ class TStoreGEMData<double>+;
+#pragma link C++ class TStoreGEMData<float>+;
+#pragma link C++ class TStoreGEMData<bool>+;
+#pragma link C++ class TStoreGEMData<int32_t>+;
+#pragma link C++ class TStoreGEMData<uint32_t>+;
+#pragma link C++ class TStoreGEMData<uint16_t>+;
+#pragma link C++ class TStoreGEMData<char>+;
+
+#pragma link C++ class  TStoreLabVIEWEvent+;
+
+#endif
+
+/* emacs
+ * Local Variables:
+ * tab-width: 8
+ * c-basic-offset: 3
+ * indent-tabs-mode: nil
+ * End:
+ */
